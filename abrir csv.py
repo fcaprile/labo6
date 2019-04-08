a331d3cf248e199df48f7efedc5e057b01847af6
Lara y Fer <fcaprile@gmail.com>
2019-04-08 16:06:14 -0700

    script para abrir los csv

--- abrir csv.py
+++ abrir csv.py
@@ -4,7 +4,26 @@ Created on Mon Apr  8 15:35:12 2019
 
 @author: Admin
 """
-
 import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+from numpy import genfromtxt
+plt.clf()
+plt.close()
 
-a=np.loadtxt(carpeta+nombre,delimiter=',')
+carpeta='C:/Users/Admin/Desktop/labo6_Rosenberg_Caprile/mediciones/4-8/'
+#carpeta='C:/Users/ferchi/Desktop/github labo 6/labo6/mediciones/27-03/'
+indice=[]
+for archivo in os.listdir(carpeta):
+    if archivo.endswith(".csv"):
+        indice.append(archivo)
+
+j=0
+nombre=indice[j]
+data = genfromtxt(carpeta+nombre, delimiter=',')
+
+data=data[1:,:]
+plt.plot(data[:,0],data[:,1])
+#b=pd.read_csv(carpeta+nombre,header=None)
+#a=np.loadtxt(carpeta+nombre,delimiter=',')
