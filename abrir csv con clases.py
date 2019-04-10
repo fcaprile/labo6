e13803d6fb50d1606f0c5f33d8471e584d6f56d9
Lara y Fer <fcaprile@gmail.com>
2019-04-10 15:30:05 -0700

    guarde la version para abrir csv que no usa clases

--- abrir csv con clases.py
+++ abrir csv con clases.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 10 15:28:59 2019
-
-@author: Admin
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Mon Apr  8 15:35:12 2019
+
 @author: Admin
 """
 import pandas as pd
@@ -15,6 +9,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import os
 from numpy import genfromtxt
+from scipy.integrate import cumtrapz as integrar
 #plt.clf()
 #plt.close()
 
@@ -25,13 +20,51 @@ for archivo in os.listdir(carpeta):
     if archivo.endswith(".csv"):
         indice.append(archivo)
 
-class Csv:
-    def __init__ (self,carpeta,numero_de_archivo):
+def filtrar_por_vecinos(y,n_vecinos):
+    yfilt=[]
+    for i in range(len(y)):
+        if i>n_vecinos and i<len(y)-n_vecinos:
+            suma=0
+            for j in range(n_vecinos):
+                suma+=y[i+j]+y[i-j]
+            yfilt.append(suma/(2*n_vecinos+1))
+        if i<n_vecinos:
+            suma=0
+            for j in range(i):
+                suma+=y[i+j]+y[i-j]
+            yfilt.append(suma/(2*i+1))
+        if i>len(y)-n_vecinos:
+            suma=0
+            for j in range(len(y)-i):
+                suma+=y[i+j]+y[i-j]
+            yfilt.append(suma/(2*(len(y)-i)+1))            
+    yfilt.append(0)
+    yfilt.append(0)
+    yfilt=np.array(yfilt)
+    return(yfilt)
+
+
+
+class Data:
+    def calibrar_bobina(self,num_vecinos=20):
+        self.y=filtrar_por_vecinos(self.y,num_vecinos)
+        self.y=integrar(self.y,self.x)
+        A=9.72*10**8
+        B=0.911
+        self.y*=A
+        self.x*=B
+    
+        
+        
+class Csv(Data):
+    def __init__ (self,carpeta,numero_de_archivo,es_bobina=False):
         nombre=indice[numero_de_archivo]        
         self.values=genfromtxt(carpeta+nombre, delimiter=',')
         self.values=self.values[1:,:]
         self.x=self.values[:,0]
         self.y=self.values[:,1]
+        if es_bobina==True:
+            self.y=calibrar_bobina(self.y)
 
     def plot(self,fig_num=0,escala=1,color='b-'):
         plt.figure(num= fig_num , figsize=(14, 10), dpi=80, facecolor='w', edgecolor='k')        
