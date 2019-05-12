916fe1740530f34abaa29a59fc7d093fa10a599a
fcaprile <fcaprile@gmail.com>
2019-05-12 00:53:23 -0300

    mejorado para la sonda y agregado el encontrar picos,

--- abrir csv con clases.py
+++ abrir csv con clases.py
@@ -13,8 +13,8 @@ from scipy.integrate import cumtrapz as integrar
 #plt.clf()
 #plt.close()
 
-carpeta='C:/Users/Admin/Desktop/labo6_Rosenberg_Caprile/mediciones/4-10/'
-carpeta='C:/Users/ferchi/Desktop/github labo 6/labo6/mediciones/4-10/20V/'
+#carpeta='C:/Users/Admin/Desktop/labo6_Rosenberg_Caprile/mediciones/4-10/'
+carpeta='C:/Users/ferchi/Desktop/github labo 6/labo6/mediciones/5-8/60/'
 indice=[]
 for archivo in os.listdir(carpeta):
     if archivo.endswith(".csv"):
@@ -23,15 +23,7 @@ for archivo in os.listdir(carpeta):
 
 
 
-class Data:
-    def calibrar_bobina(self,num_vecinos=20):
-#        self.y=filtrar_por_vecinos(self.y,num_vecinos)
-        self.y=integrar(self.y,self.x)
-        A=9.72*10**8
-        B=0.911
-        self.y*=A
-        self.x=self.x[1:]*B
-     
+class Data:        
     def sacar_offset(self,n):
         suma=0
         for valor in self.y[:n]:
@@ -59,46 +51,74 @@ class Data:
                 yfilt.append(suma/(2*(len(self.y)-i)+1))            
         yfilt.append(0)
         yfilt.append(0)
-        self.y=yfilt
+        self.y=np.array(yfilt)
 
-
+    def calibrar_bobina(self,num_vecinos=20):
+#        self.y=self.filtrar_por_vecinos(num_vecinos)
+        self.y=integrar(self.y,self.x)
+        A=9.72*10**8
+        B=0.911
+        self.y*=A
+        self.x=self.x[1:]*B
+        self.y*=-1
+        self.x-=0.7*10**-6
         
+    def encontrar_picos(self,porcentaje_altura,distancia_entre_picos=1,valle=False,plotear=False):
+        if valle==False:            
+            picos=detect_peaks(self.y,max(self.y)*porcentaje_altura,distancia_entre_picos,show=plotear)
+        if valle==True:            
+            picos=detect_peaks(self.y,min(self.y)*porcentaje_altura,distancia_entre_picos,valley=valle,show=plotear)
+        return picos
+    def sacar_lineal(self):
+        recta=np.linspace(self.y[0],self.y[-1],len(self.y))
+        self.y-=recta
+   
+
+     
 class Csv(Data):
     def __init__ (self,carpeta,numero_de_archivo,es_bobina=False):
         nombre=indice[numero_de_archivo]        
         self.values=genfromtxt(carpeta+nombre, delimiter=',')
         self.values=self.values[1:,:]
-        self.x=self.values[:,0]
-        self.y=self.values[:,1]
+        self.x=np.array(self.values[:,0])
+        self.y=np.array(self.values[:,1])
         if es_bobina==True:
             super().sacar_offset(50)
-            super().calibrar_bobina(10)
+            super().calibrar_bobina()
             
-    def plot(self,fig_num=0,escala=1,color='b-'):
-        plt.figure(num= fig_num , figsize=(14, 10), dpi=80, facecolor='w', edgecolor='k')        
+    def plot(self,fig_num=0,escala=1,tamañox=14,tamañoy=10,color='b-'):
+        plt.figure(num= fig_num , figsize=(tamañox, tamañoy), dpi=80, facecolor='w', edgecolor='k')        
         plt.plot(self.x,self.y*escala,color)
-
+        plt.grid(True) # Para que quede en hoja cuadriculada
+        plt.xlabel('Tiempo (s)')
+        plt.ylabel('Tension (V)')
+        plt.legend(loc = 'best') 
 
 def plot(x,y,fig_num=0,escala=1,color='b-'):
     plt.figure(num= fig_num , figsize=(14, 10), dpi=80, facecolor='w', edgecolor='k')        
     plt.plot(x,y*escala,color)
+    plt.grid(True) # Para que quede en hoja cuadriculada
+    plt.xlabel('Tiempo (s)')
+    plt.ylabel('Tension (V)')
+    plt.legend(loc = 'best') 
+    
     
 #%%
 exitos=np.array([2,7,9])       
 #for j in range(int(len(indice)/2)):
-for j in exitos:
-    data1=Csv(carpeta,j+1)
-    data2=Csv(carpeta,j,es_bobina=True)
-    data1.filtrar_por_vecinos(20)
-    data1.plot(fig_num=j,color='r-')
-    data2.plot(fig_num=j,escala=0.003,color='b-')
+#for j in exitos:
+j=2
 
-
-
-
-
-
-
+bobina=Csv(carpeta,j,es_bobina=True)
+resistencia=Csv(carpeta,j+1)
+resistencia.filtrar_por_vecinos(100)
+bobina.sacar_lineal()
+bobina.plot(fig_num=j,tamañox=14,tamañoy=10,color='r-')
+resistencia.plot(fig_num=j,escala=200,tamañox=14,tamañoy=10,color='b-')
+pico_bobina=bobina.encontrar_picos(0.8,distancia_entre_picos=100,valle=True)
+pico_resistencia=resistencia.encontrar_picos(0.8,distancia_entre_picos=100)
+plt.plot(bobina.x[pico_bobina],bobina.y[pico_bobina],'g*')
+plt.plot(resistencia.x[pico_resistencia],resistencia.y[pico_resistencia]*200,'g*')
 
 
 
