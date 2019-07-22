24729c871a03c938a6e756e37950a6692037e366
fcaprile <fcaprile@gmail.com>
2019-07-22 13:23:14 -0300

    faltaba otro

--- scripts/sonda langmuir/crear y analizar curva caracteristica.py
+++ scripts/sonda langmuir/crear y analizar curva caracteristica.py
@@ -9,6 +9,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 from numpy import genfromtxt
+from scipy.integrate import cumtrapz as integrar
 
 
 #falta multiplicar por la escala temporal de 2*10**6*10**-12s=2*10**-6s?
@@ -16,6 +17,80 @@ from numpy import genfromtxt
 #no hace falta multiplicar y va entre +-20+12
 
 #se asume que en todas las mediciones la sonda estaba correctamente medida en x10
+class Data:        
+    def sacar_offset(self,n):
+        suma=0
+        for valor in self.y[:n]:
+            suma+=valor
+        suma/=n
+        self.y=self.y-suma
+    
+    def filtrar_por_vecinos(self,n_vecinos=15):
+        yfilt=[]
+        for i in range(len(self.y)):
+            if i>n_vecinos and i<len(self.y)-n_vecinos:
+                suma=0
+                for j in range(n_vecinos):
+                    suma+=self.y[i+j]+self.y[i-j]
+                yfilt.append(suma/(2*n_vecinos+1))
+            if i<n_vecinos:
+                suma=0
+                for j in range(i):
+                    suma+=self.y[i+j]+self.y[i-j]
+                yfilt.append(suma/(2*i+1))
+            if i>len(self.y)-n_vecinos:
+                suma=0
+                for j in range(len(self.y)-i):
+                    suma+=self.y[i+j]+self.y[i-j]
+                yfilt.append(suma/(2*(len(self.y)-i)+1))            
+        yfilt.append(0)
+        yfilt.append(0)
+        self.y=np.array(yfilt)
+
+    def calibrar_bobina(self,num_vecinos=20):
+#        self.y=self.filtrar_por_vecinos(num_vecinos)
+        self.y=integrar(self.y,self.x)
+        A=9.72*10**8
+        B=0.911
+        self.y*=A
+        self.x=self.x[1:]*B
+        self.y*=-1
+        self.x-=0.7*10**-6
+        
+    def encontrar_picos(self,porcentaje_altura,distancia_entre_picos=1,valle=False,plotear=False):
+        if valle==False:            
+            picos=detect_peaks(self.y,max(self.y)*porcentaje_altura,distancia_entre_picos,show=plotear)
+        if valle==True:            
+            picos=detect_peaks(self.y,min(self.y)*porcentaje_altura,distancia_entre_picos,valley=valle,show=plotear)
+        return picos
+    def sacar_lineal(self):
+        recta=np.linspace(self.y[0],self.y[-1],len(self.y))
+        self.y-=recta
+   
+
+     
+class Csv(Data):
+    def __init__ (self,carpeta,numero_de_archivo,es_bobina=False):
+        indice=[]
+        for archivo in os.listdir(carpeta):
+            if archivo.endswith(".csv"):
+                indice.append(archivo)
+        nombre=indice[numero_de_archivo]        
+        self.values=genfromtxt(carpeta+nombre, delimiter=',')
+        self.values=self.values[1:,:]
+        self.x=np.array(self.values[:,0])
+        self.y=np.array(self.values[:,1])
+        if es_bobina==True:
+            super().sacar_offset(50)
+            super().calibrar_bobina()
+            
+    def plot(self,fig_num=0,escala=1,tamañox=14,tamañoy=10,color='b-'):
+        plt.figure(num= fig_num , figsize=(tamañox, tamañoy), dpi=80, facecolor='w', edgecolor='k')        
+        plt.plot(self.x,self.y*escala,color)
+        plt.grid(True) # Para que quede en hoja cuadriculada
+        plt.xlabel('Tiempo (s)')
+        plt.ylabel('Tension (V)')
+
 
 def filtrar(data):
     def butter_lowpass(cutoff, fs, order=5):
@@ -118,80 +193,6 @@ def curva_por_carpeta(carpeta_base):
     
     return tensiones,corrientes,tension_media,corriente_media,error_tension_media,error_corriente_media
 
-
-class Data:        
-    def sacar_offset(self,n):
-        suma=0
-        for valor in self.y[:n]:
-            suma+=valor
-        suma/=n
-        self.y=self.y-suma
-    
-    def filtrar_por_vecinos(self,n_vecinos=15):
-        yfilt=[]
-        for i in range(len(self.y)):
-            if i>n_vecinos and i<len(self.y)-n_vecinos:
-                suma=0
-                for j in range(n_vecinos):
-                    suma+=self.y[i+j]+self.y[i-j]
-                yfilt.append(suma/(2*n_vecinos+1))
-            if i<n_vecinos:
-                suma=0
-                for j in range(i):
-                    suma+=self.y[i+j]+self.y[i-j]
-                yfilt.append(suma/(2*i+1))
-            if i>len(self.y)-n_vecinos:
-                suma=0
-                for j in range(len(self.y)-i):
-                    suma+=self.y[i+j]+self.y[i-j]
-                yfilt.append(suma/(2*(len(self.y)-i)+1))            
-        yfilt.append(0)
-        yfilt.append(0)
-        self.y=np.array(yfilt)
-
-    def calibrar_bobina(self,num_vecinos=20):
-#        self.y=self.filtrar_por_vecinos(num_vecinos)
-        self.y=integrar(self.y,self.x)
-        A=9.72*10**8
-        B=0.911
-        self.y*=A
-        self.x=self.x[1:]*B
-        self.y*=-1
-        self.x-=0.7*10**-6
-        
-    def encontrar_picos(self,porcentaje_altura,distancia_entre_picos=1,valle=False,plotear=False):
-        if valle==False:            
-            picos=detect_peaks(self.y,max(self.y)*porcentaje_altura,distancia_entre_picos,show=plotear)
-        if valle==True:            
-            picos=detect_peaks(self.y,min(self.y)*porcentaje_altura,distancia_entre_picos,valley=valle,show=plotear)
-        return picos
-    def sacar_lineal(self):
-        recta=np.linspace(self.y[0],self.y[-1],len(self.y))
-        self.y-=recta
-   
-
-     
-class Csv(Data):
-    def __init__ (self,carpeta,numero_de_archivo,es_bobina=False):
-        indice=[]
-        for archivo in os.listdir(carpeta):
-            if archivo.endswith(".csv"):
-                indice.append(archivo)
-        nombre=indice[numero_de_archivo]        
-        self.values=genfromtxt(carpeta+nombre, delimiter=',')
-        self.values=self.values[1:,:]
-        self.x=np.array(self.values[:,0])
-        self.y=np.array(self.values[:,1])
-        if es_bobina==True:
-            super().sacar_offset(50)
-            super().calibrar_bobina()
-            
-    def plot(self,fig_num=0,escala=1,tamañox=14,tamañoy=10,color='b-'):
-        plt.figure(num= fig_num , figsize=(tamañox, tamañoy), dpi=80, facecolor='w', edgecolor='k')        
-        plt.plot(self.x,self.y*escala,color)
-        plt.grid(True) # Para que quede en hoja cuadriculada
-        plt.xlabel('Tiempo (s)')
-        plt.ylabel('Tension (V)')
 
 
 def ajustar_entre(f,x,y,ey,xinf,xsup,escalax=1,escalay=1,color='g',label='Ajuste',plot=True):
